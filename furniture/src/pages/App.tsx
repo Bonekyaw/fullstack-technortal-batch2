0593f30c7c669493da45c208b0a610e0c186d2ac
bonekyaw <phonenai2014@gmail.com>
2026-01-25 15:06:31 +0630

    better-auth

--- furniture/src/pages/App.tsx
+++ furniture/src/pages/App.tsx
@@ -1,64 +1,30 @@
-import { useState } from "react";
-import { Link } from "react-router";
-
 import { Button } from "@/components/ui/button";
-import { Spinner } from "@/components/ui/spinner";
-
-// type HeaderProps = {
-//   title: string;
-//   body: string;
-// };
-
-interface TitleProps {
-  title: string;
-}
-
-interface HeaderPropsInterface extends TitleProps {
-  body: string;
-}
-
-function Header({ title, body }: HeaderPropsInterface) {
-  return (
-    <>
-      <h1 className="text-red-500">This is {title}</h1>
-      <div>This is {body}</div>
-    </>
-  );
-}
-
-function Footer({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
-}
-
-// const TabButton = ({
-//   label,
-//   onPress,
-// }: {
-//   label: string;
-//   onPress: () => void;
-// }) => <button onClick={onPress}>{label}</button>;
+import { signUp, signIn } from "@/lib/auth-client";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const date = new Date().toLocaleDateString();
+  const handleSignUp = async () => {
+    const name = "John Doe";
+    const email = "john.doe@example.com";
+    const password = "securePassword123";
 
-  const increment = () => {
-    setCounter((prevCounter) => prevCounter + 1);
+    const { data, error } = await signUp.email({ name, email, password });
+    console.log("data", data);
+    console.log("error", error);
+  };
+
+  const handleSignIn = async () => {
+    const email = "john.doe@example.com";
+    const password = "securePassword123";
+    const { data, error } = await signIn.email({ email, password });
+    console.log("data", data);
+    console.log("error", error);
   };
 
   return (
     <>
-      <Link to="/login">Go to Login</Link>
-      <Header title="Furniture Store" body="Welcome to our furniture store!" />
-      <h2>Today is {date}</h2>
-      <button onClick={increment}>Count - {counter}</button>
-      <Button size="sm" variant="outline" disabled>
-        <Spinner className="text-orange-300" />
-        Submit
-      </Button>
-      <Footer>
-        <p>This is Footer</p>
-      </Footer>
+      <h1>Welcome to the Furniture Store</h1>
+      <Button onClick={handleSignUp}>Sign Up</Button>
+      <Button onClick={handleSignIn}>Sign In</Button>
     </>
   );
 }
